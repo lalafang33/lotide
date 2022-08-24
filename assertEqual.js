fef26319b974999403cbb074ede74ddda8d62ff8
Lalafang33 <lalafang33@gmail.com>
2022-08-24 15:39:45 -0700

    first commit

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
-if (actual === expected){
-  console.log(actual);
-} else {
-  console.assert("error");
-}
-};
+  if (actual === expected){
+    console.log(actual);
+  } else {
+    console.assert(actual == expected, "error");
+  }
+  };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
