22a116eae8ef1a126b98a57eef9d707170e3b755
Lalafang33 <lalafang33@gmail.com>
2022-08-23 21:45:58 -0700

    completed assertAraysEqaul

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1,arr2){
+  const result = eqArrays(arr1,arr2)
+    if (!result){
+        console.log(`Error: Uh Oh! These arrays are NOT equal`)
+    }
+    else {
+        console.log(`Yay! : Congratulations! These arrays ARE equal`)
+    }
+}
