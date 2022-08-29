8acbb1bbd6c99b3e60c5871838c20d7efad85673
Lalafang33 <lalafang33@gmail.com>
2022-08-29 16:43:48 -0700

    first commit

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,13 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-  assertEqual(result, ["Lighthouse", "Labs"]);
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+//   assertEqual(result, ["Lighthouse", "Labs"]);
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(assertEqual(tail(["Hello", "Lighthouse", "Labs"], ['Lighthouse', 'Labs'])));
+  });
+
+});
